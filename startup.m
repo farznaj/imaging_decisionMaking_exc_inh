72deef091a3d35b2a56e60c9ce5faae92a7c513c
gamal <gamaleldin.elsayed@gmail.com>
2016-03-11 10:25:07 -0500

    add subdirectories to the path

---
+++ startup.m
@@ -0,0 +1,11 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% <maxEntropy>
+% Copyright (C) 2016 Gamaleldin F. Elsayed and John P. Cunningham (see full notice in README)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This is a startup file which initializes Matlab's settings to
+% allow for better use of this package.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+addpath(genpath(pwd))
