61256e86eb491fb69230de1df547d8389d13c6ca
gamal <gamaleldin.elsayed@gmail.com>
2016-07-25 20:56:58 -0400

    making sure allthings are pushed

--- utils/logisticRegression/mainLogisticRegression.py
+++ utils/logisticRegression/mainLogisticRegression.py
@@ -8,6 +8,8 @@ Created on Thu Jul 21 12:18:32 2016
 import scipy
 import numpy as np
 from logisticRegression import *
+import matplotlib.pyplot as plt
+plt.close('all')
 #%% data
 
 XY = scipy.io.loadmat('/Users/gamalamin/git_local_repository/Farzaneh/utils/logisticRegression/XYbehaviour.mat', variable_names=['X', 'Y']);
@@ -23,7 +25,7 @@ X = np.random.randn((numObservations*numFeatures))
 Y = np.random.randint(0, high=2, size = numObservations)
 """
 #%% 
-l = [0.00, 0.00]
+l = [0., 0.]
 wVect = []
 for i in range(1):
     w, b, lps, perClassEr, cost, optParams = logisticRegression(X, Y, l)
