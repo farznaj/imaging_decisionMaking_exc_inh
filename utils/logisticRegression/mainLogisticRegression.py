7d9ec55ae080388f3435000da02a0e431ded87c7
gamal <gamaleldin.elsayed@gmail.com>
2016-08-12 19:59:37 -0400

    decision criteria for best reg params

--- utils/logisticRegression/mainLogisticRegression.py
+++ utils/logisticRegression/mainLogisticRegression.py
@@ -39,7 +39,7 @@ l = np.zeros((len(lvect)+1, 2))
 l[1:, 0] = lvect; # l2-regularization
 l[1:, 1] = lvect; # l1-regularization
 l = l*scale;
-numSamples = 1;  # number of samples for each regularization value (the more the better the estimate of the cross-validation error yet the slower the algorithm)
+numSamples = 2;  # number of samples for each regularization value (the more the better the estimate of the cross-validation error yet the slower the algorithm)
 
 perClassErrorTest = np.nan+np.ones((numSamples, l.shape[0]));
 perClassErrorTrain = np.nan+np.ones((numSamples, l.shape[0]));
@@ -57,16 +57,36 @@ for s in range(numSamples):
     XTrain = Xs[range(int((kfold-1.)/kfold*numObservations)), :];
     XTest = Xs[np.arange(int((kfold-1.)/kfold*numObservations), numObservations), :];
     ## %%%%% loop over the possible regularization values
-    for i in range(len(lvect)):
+    for i in range(l.shape[0]):
         w, b, lps, perClassEr, cost, optParams = logisticRegression(np.reshape(XTrain, (np.prod(XTrain.shape)), order = 'F'), YTrain, l[i, :])
         perClassErrorTest[s, i] = optParams.perClassErFn(XTest, YTest);
         perClassErrorTest[s, i] = optParams.perClassErFn(XTrain, YTrain);
+        
+        
+        #%%
+meanPerClassErrorTrain = np.mean(perClassErrorTrain, axis = 0);
+semPerClassErrorTrain = np.std(perClassErrorTrain, axis = 0)/np.sqrt(numSamples);
+
+meanPerClassErrorTest = np.mean(perClassErrorTest, axis = 0);
+semPerClassErrorTest = np.std(perClassErrorTest, axis = 0)/np.sqrt(numSamples);
+ix = np.argmin(meanPerClassErrorTest);
+ibest = l[meanPerClassErrorTest <= (meanPerClassErrorTest[ix]+semPerClassErrorTest[ix]), :];
+lbest = l[-1, :]; # best regularization term based on minError+SE criteria
+
+#%%%%%% plot coss-validation results
+plt.figure('cross validation')
+plt.errorbar(lvect, meanPerClassErrorTrain[1:], semPerClassErrorTrain[1:], 'b')
+plt.errorbar(lvect, meanPerClassErrorTest[1:], semPerClassErrorTest[1:], 'r')
+plt.plot(lvect[ibest], meanPerClassErrorTest[ibest], 'ro')
+plt.xlim([lvect[0], lvect[-1]])
+plt.xscale('log')
+plt.xlabel('regularization parameter')
+plr.ylabel('classification error (%)')
+legend('training error', 'test error', 'best parameter')
 
 
 
 #%%
-scale = np.sqrt((X**2).mean()).astype('float');
-l = scale * np.array([0. , 0.]);
-w, b, lps, perClassEr, cost, optParams = logisticRegression(np.reshape(X, (np.prod(X.shape)), order = 'F'), Y, l)
+w, b, lps, perClassEr, cost, optParams = logisticRegression(np.reshape(X, (np.prod(X.shape)), order = 'F'), Y, lbest)
 
 scio.savemat('logisticResults.mat', {'w':w, 'b': b, 'lps':lps, 'perClassEr': perClassEr, 'cost':cost, 'optParams':optParams})
