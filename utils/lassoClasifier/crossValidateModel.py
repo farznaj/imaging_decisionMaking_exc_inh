b400c28df929ff2cd23f3bc9fbe662cada45b751
gamal <gamaleldin.elsayed@gmail.com>
2016-08-16 19:49:49 -0400

    new functions for SVM model

---
+++ utils/lassoClasifier/crossValidateModel.py
@@ -0,0 +1,29 @@
+#%%
+def crossValidateModel(X, Y, modelFn, **options):
+    import numpy as np
+    import numpy.random as rng
+    if options.get('kfold'):
+        kfold = options.get('kfold');
+    else:
+        kfold = 10;
+    Y = np.squeeze(np.array(Y).astype(int));
+    if X.shape[0]>len(Y):
+        numObservations = len(Y);
+        numFeatures = len(X)/numObservations;
+        X = np.reshape(np.array(X.astype('float')), (numObservations, numFeatures), order='F');
+    
+    numObservations, numFeatures = X.shape;
+    ## %%%%%% shuffle trials to break any dependencies on the sequence of trails 
+    shfl = rng.permutation(np.arange(0, numObservations));
+    Ys = Y[shfl];
+    Xs = X[shfl, :]; 
+    ## %%%%% divide data to training and testin sets
+    YTrain = Ys[np.arange(0, int((kfold-1.)/kfold*numObservations))];
+    YTest = Ys[np.arange(int((kfold-1.)/kfold*numObservations), numObservations)];
+
+    XTrain = Xs[np.arange(0, int((kfold-1.)/kfold*numObservations)), :];
+    XTest = Xs[np.arange(int((kfold-1.)/kfold*numObservations), numObservations), :];
+
+    results = modelFn(XTrain, YTrain, XTest, YTest, options)
+    return results;
+
---
+++ utils/lassoClasifier/linearSVM.py
@@ -0,0 +1,33 @@
+def linearSVM(XTrain, YTrain, XTest, YTest, options):
+    import numpy as np
+    from sklearn import svm
+    linear_svm = [];
+    # Create a classifier: a support vector classifier
+    if options.get('l1'):
+        l1 = options.get('l1');
+        #print 'running l1 svm classification\r' 
+        linear_svm = svm.LinearSVC(C = l1, loss='squared_hinge', penalty='l1', dual=False)
+    elif options.get('l2'):
+        l2 = options.get('l2');        
+        #print 'running l2 svm classification\r' 
+        linear_svm = svm.LinearSVC(C = l2, loss='squared_hinge', penalty='l2', dual=True)
+    linear_svm.fit(XTrain, np.squeeze(YTrain))    
+    def perClassError(Y, Yhat):
+        import numpy as np
+        perClassEr = sum(abs(np.squeeze(Yhat).astype(float)-np.squeeze(Y).astype(float)))/len(Y)*100
+        return perClassEr
+    
+    perClassErrorTest = perClassError(YTest, linear_svm.predict(XTest));
+    perClassErrorTrain = perClassError(YTrain, linear_svm.predict(XTrain));
+    
+    class summaryClass:
+        perClassErrorTrain = [];
+        perClassErrorTest = [];
+        model = [];
+    summary = summaryClass();
+    summary.perClassErrorTrain = perClassErrorTrain;
+    summary.perClassErrorTest = perClassErrorTest;
+    summary.model = linear_svm;
+    return summary
+
+            
