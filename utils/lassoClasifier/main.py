250f1570c6ee979763b3a561a798974c5d74f31f
gamal <gamaleldin.elsayed@gmail.com>
2016-08-17 11:33:15 -0400

    restore the files after Farzaneh checkout

--- utils/lassoClasifier/main.py
+++ utils/lassoClasifier/main.py
@@ -12,8 +12,8 @@ import matplotlib.pyplot as plt
 from lassoLinearSVM import *
 #%% load data
 #dirName = '//sonas-hs.cshl.edu/churchland/data/fni17/imaging/151029/151029_003_ch2-PnevPanResults-160426-191859.mat';
-#dirName = '/Users/gamalamin/git_local_repository/Farzaneh/XY.mat';
-dirName = '/home/farznaj/Shares/Churchland/data/fni17/imaging/151022/XY_fni17_151022 XY_lassoSVM.mat';
+dirName = '/Users/gamalamin/git_local_repository/Farzaneh/XY.mat';
+#dirName = '/home/farznaj/Shares/Churchland/data/fni17/imaging/151022/XY_fni17_151022 XY_lassoSVM.mat';
 XY = scipy.io.loadmat(dirName, variable_names=['X', 'Y']);
 X = XY.pop('X')
 Y = XY.pop('Y')
@@ -22,7 +22,12 @@ meanX = np.mean(X, axis = 0);
 stdX = np.std(X, axis = 0);
 X = (X-meanX)/stdX;
 
-X = np.reshape(X, (X.shape[0]*X.shape[1], 1), order='F');
+X = np.reshape(X, (X.shape[0]*X.shape[1], 1), order='F');#%% do cross validation and pick the regularization parameter
+#%%
+kfold = 10
+numSamples = 2
+lbest = crossValidateSVM(X, Y, 'l2', kfold, numSamples)
+
 #%% run lasso SVM
 wl1_shuff, b_shuff, wl1, b, bestCVerror, bestc, linear_svm, meanCVerror, semCVerror, c, meanX, stdX = lassoLinearSVM(X, Y, 10)
 
