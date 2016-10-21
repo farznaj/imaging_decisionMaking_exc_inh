c9aa549888821fc428b3ba6670f0d5b1c41ad874
gamaleldin <gamaleldin.elsayed@gmail.com>
2016-10-21 10:54:21 -0400

    return the magnitude

--- utils/TDR/optimize_oTDR.m
+++ utils/TDR/optimize_oTDR.m
@@ -1,4 +1,4 @@
-function [Q] = optimize_oTDR(dataTensor, codedParams, trCountmtx, spcConstraint)
+function [Q, g] = optimize_oTDR(dataTensor, codedParams, trCountmtx, spcConstraint)
 % It takes all the times and tries to find the best axes.
 % Q: neurons x numberTunedFeatures : beta coefficients for all predictors except the offset.
 
@@ -17,7 +17,7 @@ end
 numSignals = size(codedParams, 2);
 Q = orth(randn(N, numSignals-1));
 Beta0 = randn(N, T);
-a = ones(T, numSignals-1);
+g = ones(T, numSignals-1);
 
 %% random initialization within data space
 maxIter = 200;
@@ -25,7 +25,7 @@ stp = 10000;
 for i = 1:maxIter
     i
     for t = randperm(T)
-        Betas = [Q*diag(a(t,:)) Beta0(:,t)];
+        Betas = [Q*diag(g(t,:)) Beta0(:,t)];
         [f(i,t), gradf, ~,~] = TDRobjectiveFn(dataTensor(t, :, :), codedParams, permute(Betas, [3 1 2]), trCountmtx);
         gradf = squeeze(gradf);
         %         Betas(:,j) = Betas(:,j)-stp*gradf(:,j); % update gradient with respect to data R{j}
@@ -34,7 +34,7 @@ for i = 1:maxIter
         Q = Betas(:, 1:numSignals-1); % neurons x numTunedFeatures
         Beta0(:,t) = Betas(:, numSignals);
         [u,~,v] = svd(projSpc*Q, 0); %u: neurons x numTunedFeatures ; v: numTunedFeatures x numTunedFeatures
-        a(t,:) = diag((Q')*(u*v')); % diag((Q')*(u*v')) is of size numTunedFeatures x numTunedFeatures
+        g(t,:) = diag((Q')*(u*v')); % diag((Q')*(u*v')) is of size numTunedFeatures x numTunedFeatures
         Q = (u*v'); % u*v’ is the nearest orthonormal matrix.... the closest projection to the space of orthonormal matrices
     end
     %%
