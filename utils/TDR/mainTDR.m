deaa408634a1be861e29470cf7aada6c9ff58e7a
gamaleldin <gamaleldin.elsayed@gmail.com>
2016-10-27 21:47:26 -0400

    add neurons exclusion criteria as farzaneh, smooth data

--- utils/TDR/mainTDR.m
+++ utils/TDR/mainTDR.m
@@ -51,8 +51,10 @@ postNProvided = 1; % whether the directory cotains the postFile or not. if it do
 [imfilename, pnevFileName] = setImagingAnalysisNames(mouse, imagingFolder, mdfFileNumber, signalCh, pnev2load, postNProvided);
 [pd, pnev_n] = fileparts(pnevFileName);
 postName = fullfile(pd, sprintf('post_%s.mat', pnev_n));
+moreName = fullfile(pd, sprintf('more_%s.mat', pnev_n));
 
 load(postName, 'stimAl_allTrs', 'stimrate', 'time1stSideTry', 'timeCommitCL_CR_Gotone', 'timeStimOnset', 'timeStimOffset', 'outcomes', 'allResp_HR_LR', 'cb')
+load(moreName, 'inhibitRois')
 % load('SVM_151029_003_ch2-PnevPanResults-160426-191859.mat')
 
 
@@ -85,10 +87,68 @@ traces_al_stim = traces_al_stim(:, ~NsExcluded, ~trsExcluded);
 stimrate = stimrate(~trsExcluded);
 
 
+%%   
+inhRois = inhibitRois(~NsExcluded);
 
 
 
+% Then I identify a random subset of neurons ( = 0.95 * numTrials):
+% ##  If number of neurons is more than 95% of trial numbers, identify n random neurons, where n= 0.95 * number of trials. This is to make sure we have more observations (trials) than features (neurons)
 
+nTrs = size(X, 1);
+nNeuronsOrig = size(X, 2);
+nNeuronsNow = floor(nTrs * .95);
+
+if nNeuronsNow < nNeuronsOrig
+    fractInh = sum(inhRois==1) / (nNeuronsOrig);
+    fractExc = sum(inhRois==0) / (nNeuronsOrig);
+    fractUn = sum(isnan(inhRois)) / (nNeuronsOrig);
+    sprintf( 'Number: inhibit = %d, excit = %d, unsure = %d', sum(inhRois==1), sum(inhRois==0), sum(isnan(inhRois)))
+    sprintf(' Fraction: inhibit = %.2f, excit = %.2f, unsure = %.2f', fractInh, fractExc, fractUn)
+% Define how many neurons you need to pick from each pool of inh, exc, unsure.
+nInh = ceil(fractInh*nNeuronsNow);
+nExc = ceil(fractExc*nNeuronsNow);
+nUn = nNeuronsNow - (nInh + nExc); % fractUn*nNeuronsNow
+
+sprintf ('\nThere are %d trials; So selecting %d neurons out of %d ', nTrs,nNeuronsNow, nNeuronsOrig)
+sprintf('%i, %i, %i: number of selected inh, excit, unsure' ,nInh, nExc, nUn)
+
+% Select nInh random indeces out of the inhibitory pool
+inhI = find(inhRois==1);
+inhNow = inhI(randperm(length(inhI)));inhNow = sort(inhNow(1:nInh)); % # random indices
+
+
+% Select nExc random indeces out of the excitatory pool
+excI = find(inhRois==0);
+excNow = excI(randperm(length(excI)));excNow = sort(excNow(1:nExc)); % # random indices
+
+
+% Select nUn random indeces out of the unsure pool
+unI = find(isnan(inhRois));
+unNow = unI(randperm(length(unI)));unNow = sort(unNow(1:nUn)); % # random indices
+
+% Put all the 3 groups together 
+neuronsNow = sort([inhNow(:); excNow(:); unNow(:)]);
+
+
+% Define a logical array with 1s for randomly selected neurons (length = number of neurons in X (after excluding NsExcluded))
+NsRand = 1:size(X,2);
+NsRand = ismember(NsRand, neuronsNow);
+    
+else % if number of neurons is already <= .95*numTrials, include all neurons.
+    sprintf('Not doing random selection of neurons (nNeurons=%d already fewer than .95*nTrials=%d)', size(X,2), nTrs)
+    NsRand = true(size(X,2));
+end
+    
+
+
+% Finally I extract only NsRand from X and inhRois:
+% Set X and inhRois only for the randomly selected set of neurons
+
+X = X(:,NsRand);
+inhRois = inhRois(NsRand);
+traces_al_stim = traces_al_stim(:, NsRand, :);
+%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%
@@ -117,13 +177,19 @@ stdN = std(reshape(permute(dataTensor, [1 3 2]), T*R, N));
 meanN = mean(X); % X: trials x neurons; includes average of window ep (defined in SVM codes) for each neuron at each trial.
 stdN = std(X);
 dataTensor = bsxfun(@times, bsxfun(@minus, dataTensor, meanN), 1./(stdN+sqrt(0))); 
+smooth_dataTensor = nan(size(dataTensor));
+for i = 1: size(dataTensor,3)
+    for j = 1:size(dataTensor,2)
+        smooth_dataTensor(:, j , i) = gaussFilter(3, dataTensor(:,j,i));
+    end
+end
 
 %% plot average of dataTensor per decision (average across trials and neurons)
 figure;
 hold on
-plot(all_times, mean(mean(dataTensor(:, :, Y==0), 3), 2), 'b')
-plot(all_times, mean(mean(dataTensor(:, :, Y==1), 3), 2), 'r')
-plot(all_times, mean(mean(dataTensor(:, :, :), 3), 2), 'k')
+plot(all_times, mean(mean(smooth_dataTensor(:, :, Y==0), 3), 2), 'b')
+plot(all_times, mean(mean(smooth_dataTensor(:, :, Y==1), 3), 2), 'r')
+plot(all_times, mean(mean(smooth_dataTensor(:, :, :), 3), 2), 'k')
 legend('average low rate', 'average high rate', 'average all')
 xlabel('time (ms)') % since stimulus onset.
 ylabel('normalized firing rates')
@@ -298,72 +364,73 @@ plot(var(squeeze(dataTensor_proj(:,1,:))), var(squeeze(dataTensor_proj(:,2,:))))
 xlabel('mean activity (stim dim)')
 ylabel('mean activity (decision dim)')
 axis square
-%% orthogonal projections 
-[sRA, g] = optimize_oTDR(dataTensor, codedParams, [], []);
-
-figure;
-hold on
-plot(all_times, g(:,1), 'r')
-plot(all_times, g(:,2), 'b')
-hold off
-xlabel('time (ms)') 
-ylabel('magnitude')
-legend('stimulus', 'decision')
-
-dataTensor_proj(:, 1, :) = projectTensor(dataTensor, squeeze(sRA(:, 1)));
-dataTensor_proj(:, 2, :) = projectTensor(dataTensor, squeeze(sRA(:, 2)));
-
-uniqueStim = unique(stim);
-uniqueDecision = unique(decision);
-S = length(uniqueStim);
-D = length(uniqueDecision);
-projStim = [];
-for s = 1:S
-    for d = 1:D
-        msk = (stim == uniqueStim(s)) & (decision == uniqueDecision(d));
-        proj1(:, s, d) = mean(squeeze(dataTensor_proj(: ,1, msk)), 2);
-        proj2(:, s, d) = mean(squeeze(dataTensor_proj(: ,2, msk)), 2);        
-    end
-end
-
-clr = redgreencmap(S, 'interpolation', 'linear');
-figure;
-l = {};
-subplot(121)
-hold on
-for s = 1:S
-    plot(all_times, proj1(:, s, 1), '--', 'color', clr(s, :));
-    h(s) = plot(all_times, proj1(:, s, 2), '-', 'color', clr(s, :));
-    l{s} = [num2str(uniqueStim(s)) 'Hz']; 
-end
-ylabel('stimulus projection')
-legend(h, l)
-xlabel('time (ms)') % since stimulus onset.
-
- 
-subplot(122)
-hold on
-for s = 1:S
-    plot(all_times, proj2(:, s, 1), '--', 'color', clr(s, :));
-    h(s) = plot(all_times, proj2(:, s, 2), '-', 'color', clr(s, :));
-end
-ylabel('decsison projection')
-legend(h, l)
-xlabel('time (ms)') % since stimulus onset.
-
-
-figure
-subplot(121)
-plot(mean(squeeze(dataTensor_proj(:,1,:))), mean(squeeze(dataTensor_proj(:,2,:))))
-xlabel('mean activity (stim dim)')
-ylabel('mean activity (decision dim)')
-axis square
-
-subplot(122)
-plot(var(squeeze(dataTensor_proj(:,1,:))), var(squeeze(dataTensor_proj(:,2,:))))
-xlabel('mean activity (stim dim)')
-ylabel('mean activity (decision dim)')
-axis square
+%%
+% % % % % % %% orthogonal projections 
+% % % % % % [sRA, g] = optimize_oTDR(dataTensor, codedParams, [], []);
+% % % % % % 
+% % % % % % figure;
+% % % % % % hold on
+% % % % % % plot(all_times, g(:,1), 'r')
+% % % % % % plot(all_times, g(:,2), 'b')
+% % % % % % hold off
+% % % % % % xlabel('time (ms)') 
+% % % % % % ylabel('magnitude')
+% % % % % % legend('stimulus', 'decision')
+% % % % % % 
+% % % % % % dataTensor_proj(:, 1, :) = projectTensor(dataTensor, squeeze(sRA(:, 1)));
+% % % % % % dataTensor_proj(:, 2, :) = projectTensor(dataTensor, squeeze(sRA(:, 2)));
+% % % % % % 
+% % % % % % uniqueStim = unique(stim);
+% % % % % % uniqueDecision = unique(decision);
+% % % % % % S = length(uniqueStim);
+% % % % % % D = length(uniqueDecision);
+% % % % % % projStim = [];
+% % % % % % for s = 1:S
+% % % % % %     for d = 1:D
+% % % % % %         msk = (stim == uniqueStim(s)) & (decision == uniqueDecision(d));
+% % % % % %         proj1(:, s, d) = mean(squeeze(dataTensor_proj(: ,1, msk)), 2);
+% % % % % %         proj2(:, s, d) = mean(squeeze(dataTensor_proj(: ,2, msk)), 2);        
+% % % % % %     end
+% % % % % % end
+% % % % % % 
+% % % % % % clr = redgreencmap(S, 'interpolation', 'linear');
+% % % % % % figure;
+% % % % % % l = {};
+% % % % % % subplot(121)
+% % % % % % hold on
+% % % % % % for s = 1:S
+% % % % % %     plot(all_times, proj1(:, s, 1), '--', 'color', clr(s, :));
+% % % % % %     h(s) = plot(all_times, proj1(:, s, 2), '-', 'color', clr(s, :));
+% % % % % %     l{s} = [num2str(uniqueStim(s)) 'Hz']; 
+% % % % % % end
+% % % % % % ylabel('stimulus projection')
+% % % % % % legend(h, l)
+% % % % % % xlabel('time (ms)') % since stimulus onset.
+% % % % % % 
+% % % % % %  
+% % % % % % subplot(122)
+% % % % % % hold on
+% % % % % % for s = 1:S
+% % % % % %     plot(all_times, proj2(:, s, 1), '--', 'color', clr(s, :));
+% % % % % %     h(s) = plot(all_times, proj2(:, s, 2), '-', 'color', clr(s, :));
+% % % % % % end
+% % % % % % ylabel('decsison projection')
+% % % % % % legend(h, l)
+% % % % % % xlabel('time (ms)') % since stimulus onset.
+% % % % % % 
+% % % % % % 
+% % % % % % figure
+% % % % % % subplot(121)
+% % % % % % plot(mean(squeeze(dataTensor_proj(:,1,:))), mean(squeeze(dataTensor_proj(:,2,:))))
+% % % % % % xlabel('mean activity (stim dim)')
+% % % % % % ylabel('mean activity (decision dim)')
+% % % % % % axis square
+% % % % % % 
+% % % % % % subplot(122)
+% % % % % % plot(var(squeeze(dataTensor_proj(:,1,:))), var(squeeze(dataTensor_proj(:,2,:))))
+% % % % % % xlabel('mean activity (stim dim)')
+% % % % % % ylabel('mean activity (decision dim)')
+% % % % % % axis square
 %%
 saving_directory = ['/Users/gamalamin/git_local_repository/Farzaneh/results/' imagingFolder '/'];
 saveFig2Directory(saving_directory)
